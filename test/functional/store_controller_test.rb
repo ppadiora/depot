3e94fbfeccc8285af2357adf9aff8842d7f50b88
Patricia Jara <gabinetecreativo@gmail.com>
2013-11-20 20:59:06 +0000

    Catalog

---
+++ test/functional/store_controller_test.rb
@@ -0,0 +1,16 @@
+require 'test_helper'
+
+class StoreControllerTest < ActionController::TestCase
+  test "should get index" do
+    get :index
+    assert_response :success
+    # este test comprueba que la página index se encuentra y nos da una respuesta positiva :success.
+    # ahora añadimos nuevo; que además la respuesta nos confirme que existe una plantilla, 
+    # la info de los productos y un precio.
+    assert_select '#columns #side a' , :minimum => 4 # menú de la izquierda debe tener 4 = home, questions, news, contact
+		assert_select '#main .entry', 4 # numero de productos en la lista
+		assert_select 'h3' , 'Programming Ruby 1.9'
+		assert_select '.price' , /\$[,\d]+\.\d\d/
+  end
+
+end
