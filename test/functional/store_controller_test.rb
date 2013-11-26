2711b602abf9c21d731bb09d7a10922c64c9315a
Patricia Jara <gabinetecreativo@gmail.com>
2013-11-26 19:05:05 +0000

    smart cart

--- test/functional/store_controller_test.rb
+++ test/functional/store_controller_test.rb
@@ -8,8 +8,8 @@ class StoreControllerTest < ActionController::TestCase
     # ahora añadimos nuevo; que además la respuesta nos confirme que existe una plantilla, 
     # la info de los productos y un precio.
     assert_select '#columns #side a' , :minimum => 4 # menú de la izquierda debe tener 4 = home, questions, news, contact
-		assert_select '#main .entry', 4 # numero de productos en la lista
-		assert_select 'h3' , 'Programming Ruby 1.9'
+	assert_select '#main .entry', 4 # numero de productos en la lista
+	assert_select 'h3' , 'Programming Ruby 1.9'
 		# assert_select '.price' , /\$[,\d]+\.\d\d/ -> 
     # quito el simbolo del dollar para que no me de problemas en el index de store/index.html 
     # en la parte number_to_currency :unit => "€" 
